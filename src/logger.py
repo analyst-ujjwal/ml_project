92136b5af3ff22b9a4bed382aaadfda54b4f40d2
Ujjwal Kumar <ujjwalkashyap1256@gmail.com>
2025-09-22 09:41:01 +0530

    Data Ingestion

--- src/logger.py
+++ src/logger.py
@@ -2,22 +2,16 @@ import logging
 import os
 from datetime import datetime
 
-# Name of the log file
-LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
 
-# Create a "logs" folder in the current working directory
-logs_dir = os.path.join(os.getcwd(), "logs")
-os.makedirs(logs_dir, exist_ok=True)
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
-# Full path to the log file
-LOG_FILE_PATH = os.path.join(logs_dir, LOG_FILE)
-
-# Configure logging
 logging.basicConfig(
     filename=LOG_FILE_PATH,
-    format="[%(asctime)s] [%(lineno)d] %(name)s - %(levelname)s - %(message)s",
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
-)
 
-if __name__ == "__main__":
-    logging.info("Divide by Zero error")
+
+)
